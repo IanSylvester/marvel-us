5173f7f3f4db4eb7628fd7477ec1644c26597463
ian <sylvester16@gmail.com>
2022-05-29 14:59:50 -0400

    formatted files, replace api with corrected end-point, fixed error for event listener

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const namesList = document.getElementById("names-list");
-const newUserForm = document.getElementById("new=user-form")
-const fName= document.getElementById("fname")
-const lName= document.getElementById("lname")
-const createPassword= document.getElementById("create-password")
-const submit= document.getElementById("submit")
-
-
-
+const newUserForm = document.getElementById("new-user-form");
+const fName = document.getElementById("fname");
+const lName = document.getElementById("lname");
+const newUserName = document.getElementById("new-user-name");
+const createPassword = document.getElementById("create-password");
+const submit = document.getElementById("submit");
 
 const fetchPeople = () => {
-  fetch("http://localhost:3000/user")
+  fetch("http://localhost:3000/users")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       renderUsers(data);
     });
+};
+
+const createPerson = (data) => {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    console.log(response);
+  });
 };
 
 fetchPeople();
@@ -25,5 +35,16 @@ const renderUsers = (arr) => {
     let li = document.createElement("li");
     li.innerText = `${element.firstName} ${element.lastName}`;
     namesList.append(li);
-    });
+  });
 };
+
+newUserForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newUser = {
+    firstName: fName.value,
+    lastName: lName.value,
+    userName: newUserName.value,
+    password: password.value,
+  };
+  console.log(newUser);
+});

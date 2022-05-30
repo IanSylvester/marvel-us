498be32a29e96b285a182275285ee3b3cfc4b6b1
ian <sylvester16@gmail.com>
2022-05-29 20:55:35 -0400

    added welcome screen, added functions for login

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const namesList = document.getElementById("names-list");
+
+//const for newUserForm
 const newUserForm = document.getElementById("new-user-form");
 const fName = document.getElementById("fname");
 const lName = document.getElementById("lname");
 const newUserName = document.getElementById("new-user-name");
 const createPassword = document.getElementById("create-password");
 const submit = document.getElementById("submit");
+//const for loginForm
+const loginForm = document.getElementById("login-form");
+const checkUser = document.getElementById("check-user");
+const checkPassword = document.getElementById("check-password");
+const login = document.getElementById("login");
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch("http://localhost:3000/users")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let result = data.find((element) => {
+        return element.userName == e.target[0].value;
+      });
+      if (result) {
+        if (result.password == e.target[1].value) {
+          window.location.href =
+            "file://wsl.localhost/Ubuntu-22.04/root/code/blogger/home.html";
+        }
+      }
+    });
+});
 const fetchPeople = () => {
   fetch("http://localhost:3000/users")
     .then((response) => {
@@ -44,7 +69,7 @@ newUserForm.addEventListener("submit", (e) => {
     firstName: fName.value,
     lastName: lName.value,
     userName: newUserName.value,
-    password: password.value,
+    password: createPassword.value,
   };
-  console.log(newUser);
+  createPerson(newUser);
 });
